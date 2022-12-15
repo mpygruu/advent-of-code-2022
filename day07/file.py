d42aee4eb882c37fa232e81f8378583c79a083ae
mpygru <barkwa67@gmail.com>
2022-12-15 18:10:36 +0100

    start from beginning with different approach

--- day07/file.py
+++
@@ -1,8 +0,0 @@
-class File:
-    def __init__(self, name: str, size: int, path: str):
-        self.name = name
-        self.size = size
-        self.path = path
-
-    def __str__(self) -> str:
-        return f"File with name: {self.name}, size: {self.size}, path: {self.path}"
