32b0dcb9e426886ceb91ef9122837850cdd5b970
mpygru <barkwa67@gmail.com>
2022-12-15 12:57:55 +0100

    add day 13 part 1,2 solutions

--- day13/solution.py
+++ day13/solution.py
@@ -2,7 +2,7 @@ INPUT_PATH = "day13/input.txt"
 EXAMPLE_INPUT_PATH = "day13/example_input.txt"
 
 def load_input_data():
-    with open(EXAMPLE_INPUT_PATH) as file:
+    with open(INPUT_PATH) as file:
         file_data = file.read().splitlines()
 
     return file_data
@@ -13,39 +13,73 @@ def decode_line(line: str):
     return json.loads(line)
 
 
-def is_in_right_order(first, second) -> bool:
-    for i in range(min(len(first), len(second))):
+def is_in_right_order(first_packet, second_packet) -> bool:
+    # -1 -> brak decyzji
+    # 0 -> False
+    # 1 -> True
+    result = -1
 
-        if isinstance(first[i], int) and isinstance(second[i], int):       
-            if first[i] == second[i]:
-                continue
-            else:
-                return (first < second)
+    for i in range(0, max(len(first_packet), len(second_packet))):
+        if i >= len(first_packet):
+            return True
+        if i >= len(second_packet):
+            return False
 
-        if isinstance(first[i], list) and isinstance(second[i], list):       
-            return is_in_right_order(first[i], second[i])
+        if is_integer(first_packet[i]) and is_integer(second_packet[i]):
+            if first_packet[i] != second_packet[i]:
+                result = first_packet[i] < second_packet[i]
+        
+        if is_list(first_packet[i]) and is_list(second_packet[i]):
+            result = is_in_right_order(first_packet[i], second_packet[i])
 
-        if isinstance(first[i], int) and isinstance(second[i], list):  
-            temp = [first[i]]    
-            return is_in_right_order(temp, second[i])
+        if is_integer(first_packet[i]) and is_list(second_packet[i]):
+            first_packet[i] = [first_packet[i]]
+            result = is_in_right_order(first_packet[i], second_packet[i])
 
-        if isinstance(first[i], list) and isinstance(second[i], int):  
-            temp = [second[i]]    
-            return is_in_right_order(temp, second[i])
-    
-    return len(first) < len(second)
+        if is_integer(second_packet[i]) and is_list(first_packet[i]):
+            second_packet[i] = [second_packet[i]]
+            result = is_in_right_order(first_packet[i], second_packet[i])
+        
+        if result != -1:
+            return result
+        
+    return -1
+        
+
+is_integer = lambda value: isinstance(value, int)
+is_list = lambda value: isinstance(value, list)
 
 
 def count_pairs_in_right_order(data: list):
     sum = 0
-
-    for i in range(0, len(data)-2, 3):
-        if is_in_right_order(decode_line(data[i]), decode_line(data[i+1])):
-            sum += 1
+    index = 1
+    for i in range(0, len(data), 3):
+        if is_in_right_order(decode_line(data[i]), decode_line(data[i+1])) == 1:
+            sum += index
+        index += 1
 
     return sum
 
+def count_decoder_key(data: list):
+    divider1 = 0
+    divider2 = 0
+    index = 1
+    for i in range(0, len(data)):
+        if len(data[i]) == 0:
+            index += 1
+            continue
+
+        if is_in_right_order(decode_line(data[i]), [[2]]) == 1:
+            divider1 += 1
+
+        if is_in_right_order(decode_line(data[i]), [[6]]) == 1:
+           divider2 += 1
+
+        index += 1
+
+    return (1+divider1)*(2+divider2)
 
 
 file_data = load_input_data()
 print("Part 1:", count_pairs_in_right_order(file_data))
+print("Part 2:", count_decoder_key(file_data))
